9e7ca9b6a991063e28b1e466fa4a76551d2edb9d
bkphan <bkphan.me@gmail.com>
2025-11-27 14:52:50 +0700

    chore: Integrate the 'Redux' Provider to the application

--- clicknext-bank-web-apps/src/main.jsx
+++ clicknext-bank-web-apps/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { store } from './stores/store.js'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
