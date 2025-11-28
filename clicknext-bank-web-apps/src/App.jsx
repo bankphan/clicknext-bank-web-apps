958b29746d77bde9204082adb80f9072b71c29a5
bkphan <bkphan.me@gmail.com>
2025-11-28 14:49:13 +0700

    chore: Route All to the main Apps

--- clicknext-bank-web-apps/src/App.jsx
+++ clicknext-bank-web-apps/src/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './pages/LoginPage.jsx'
-import DashboardPage from './pages/DashboardPage.jsx'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import LoginPage from './pages/loginPage.jsx'
+import DepositWithdrawPage from './pages/DepositWithdrawPage.jsx'
+import MainLayout from './layouts/MainLayout.jsx'
+import TransactionPage from './pages/TransactionPage.jsx'
 
+const PrivateRoute = ({ children }) => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+  return isLoggedIn ? <MainLayout>{children}</MainLayout> : <Navigate to="/login" />;
+};
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/deposit" element={<PrivateRoute><DepositWithdrawPage /></PrivateRoute>} />
+        <Route path="/transaction" element={<PrivateRoute><TransactionPage /></PrivateRoute>} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
